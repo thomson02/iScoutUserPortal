5143ae69435f236f1ee4b1146287c22b6200ff52
Andrew Thomson <thomson02@hotmail.com>
2012-03-10 10:30:44 +0000

    test web page

--- web.js
+++ web.js
@@ -3,21 +3,10 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  response.render('index'); //.send('Hello World!');
 });
 
 var port = process.env.PORT || 3000;
 app.listen(port, function() {
   console.log("Listening on " + port);
-});
-
-
-var pg = require('pg');
-
-pg.connect(process.env.DATABASE_URL, function(err, client) {
-  var query = client.query('SELECT * FROM your_table');
-
-  query.on('row', function(row) {
-    console.log(JSON.stringify(row));
-  });
 });
