462c8c29471dd8e8ea7fa14cc9ed1ab60696b701
Andrew Thomson <thomson02@hotmail.com>
2012-03-10 10:19:27 +0000

    added postgres DB

--- web.js
+++ web.js
@@ -10,3 +10,14 @@ var port = process.env.PORT || 3000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
+
+var pg = require('pg');
+
+pg.connect(process.env.DATABASE_URL, function(err, client) {
+  var query = client.query('SELECT * FROM your_table');
+
+  query.on('row', function(row) {
+    console.log(JSON.stringify(row));
+  });
+});
