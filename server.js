6233f7b9c063c856a66b929582b7a67cb822f366
Andrew Thomson <thomson02@hotmail.com>
2012-06-17 23:02:23 +0100

    rework

--- server.js
+++ server.js
@@ -1,15 +1,12 @@
-// Module dependencies.
 var express = require('express');
 
-var app = express.createServer();
+var app = express.createServer(express.logger());
 
-// Configuration
-app.configure( function() {
+app.get('/', function(request, response) {
+    response.send('Hello World!');
 });
 
-// Routes
-app.get('/', function(req, res) {
-    res.send('Hello World');
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+    console.log("Listening on " + port);
 });
-
-app.listen(3000);
