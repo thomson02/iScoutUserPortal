ae687fb15680931705007fe827dc7c7024079eb4
Andrew Thomson <thomson02@hotmail.com>
2012-06-18 00:17:33 +0100

    fix

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var app = express.createServer(express.logger());
 app.configure(function(){
         app.set("view options", { layout: false, pretty: true });
         app.use(express.favicon());
-        app.use("/public", express.static(__dirname + '/public'));
+        app.use(express.static(__dirname + '/public'));
     }
 );
 
