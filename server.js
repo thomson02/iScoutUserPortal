fbabc88a25cb25f1cb4f9fb580f1b2684545018b
Andrew Thomson <thomson02@hotmail.com>
2012-06-17 23:06:26 +0100

    new server path

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ app.get('/', function(request, response) {
     response.send('Hello World!');
 });
 
+app.get('/hello/:name', function(request, response){
+    request.send('hello ' + request.params.name);
+});
+
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
     console.log("Listening on " + port);
