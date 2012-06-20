bc4f46600a3093805dc50097eec268a260897a9a
Andrew Thomson <thomson02@hotmail.com>
2012-06-20 22:50:56 +0100

    typo

--- public/scripts/lib/require/jqueryWithBootstrap.js
+++ public/scripts/lib/require/jqueryWithBootstrap.js
@@ -1,7 +1,7 @@
 define([
     // Load the original jQuery source file
     'order!lib/require/jQuery',
-    'order!lib/jquery/bootstrap'
+    'order!lib/bootstrap/bootstrap'
 ], function($) {
     // Tell Require.js that this module returns a reference to bootstrap
     return $;
