f9e7d3a54d50d75a739a1dbfd3e8363060b2d560
Andrew Thomson <thomson02@hotmail.com>
2012-06-20 22:45:16 +0100

    argh!

--- public/scripts/lib/require/jQueryWithBootstrap.js
+++ public/scripts/lib/require/jQueryWithBootstrap.js
@@ -1,7 +1,6 @@
 define([
     // Load the original jQuery source file
-    'order!lib/require/jQuery',
-    'order!lib/bootstrap/bootstrap'
+    'order!lib/jquery/jquery-1.7.2.min'
 ], function($) {
     // Tell Require.js that this module returns a reference to bootstrap
     return $;
