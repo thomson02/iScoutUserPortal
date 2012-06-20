214b8f6667c63ebd549329d4878b3333540e75b0
Andrew Thomson <thomson02@hotmail.com>
2012-06-20 23:01:34 +0100

    fix

--- public/scripts/lib/require/charts.js
+++ public/scripts/lib/require/charts.js
@@ -5,7 +5,7 @@ define([
     'order!lib/charts/jquery.flot',
     'order!lib/charts/jquery.flot.orderBars',
     'order!lib/charts/jquery.flot.pie',
-    'order!lib/charts/jquery.flot.resize',
+    'order!lib/charts/jquery.flot.resize'
 ], function($) {
     // Tell Require.js that this module returns a reference to bootstrap
     return $;
